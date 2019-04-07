4192c949ee1e3ff06a8782f944af8ea2ed1d70b7
Nicholas Reimherr <nickryan07@sbcglobal.net>
2019-04-06 22:30:08 -0500

    app icon and splash screen

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <StatusBar barStyle="light-content"/>
+                <Expo.AppLoading />
+            </React.Fragment>
+        // <View style={styles.container}>
+        //     <Text>Open up App.js to start working on your app!</Text>
+        // </View>
+        );
+    }
+}
